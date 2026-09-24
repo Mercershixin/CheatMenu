acdb7b7ac99b98ba4fbd3b16d804cfa303abfb52
Mercershixin <56727576+Mercershixin@users.noreply.github.com>
2026-09-24 17:42:12 +0800

    update CheatMenu.lua (638455 bytes, sha1 43d8143a12d1)

--- CheatMenu.lua
+++ CheatMenu.lua
@@ -1,4 +1,4 @@
-print(('[CheatMenu] build 2026-09-24 17:34 sha 667d9e6f bytes 638338'):format('2026-09-24 17:34','667d9e6f',638338))
+print(('[CheatMenu] build 2026-09-24 17:42 sha 7a5327f0 bytes 638338'):format('2026-09-24 17:42','7a5327f0',638338))
 print("[CheatMenu] ===== v68 加载开始 =====")
 local GENV
 do local ok,e=pcall(getgenv) GENV=(ok and type(e)=="table") and e or _G end
@@ -130,7 +130,7 @@ SavedPos={},Loops={},BtnRefs={},SwitchOnChange={},Pages={},
 ScreenGui=nil,MenuOpen=false,FreeCamActive=false,MenuPrevMouseBehav=nil,MenuPrevMouseIcon=nil,
 FCPrevBehav=nil,FCPrevIcon=nil,
 }
-SYS.BuildVer="11.6.1"
+SYS.BuildVer="11.6.2"
 SYS.BuildURL="https://raw.githubusercontent.com/Mercershixin/CheatMenu/main/CheatMenu.lua"
 SYS.BuildVerURL="https://raw.githubusercontent.com/Mercershixin/CheatMenu/main/version.txt"
 SYS.FallbackRepo="https://raw.githubusercontent.com/Mercershixin/CheatMenu/main/CheatMenu.lua"
