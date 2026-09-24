5b297c03bb623515e10eb52cf4010fc49d6bbaa3
Mercershixin <56727576+Mercershixin@users.noreply.github.com>
2026-09-24 18:00:08 +0800

    update CheatMenu.lua (638455 bytes, sha1 cb4c2a317d9d)

--- CheatMenu.lua
+++ CheatMenu.lua
@@ -1,4 +1,4 @@
-print(('[CheatMenu] build 2026-09-24 17:55 sha b96a1d9d bytes 638338'):format('2026-09-24 17:55','b96a1d9d',638338))
+print(('[CheatMenu] build 2026-09-24 18:00 sha b96a1d9d bytes 638338'):format('2026-09-24 18:00','b96a1d9d',638338))
 print("[CheatMenu] ===== v68 加载开始 =====")
 local GENV
 do local ok,e=pcall(getgenv) GENV=(ok and type(e)=="table") and e or _G end
